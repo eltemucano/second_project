8700f69a8aadbd2b6efc4eb8efb87aeddae2ec5c
eltemucano <crosales.temuco@gmail.com>
2021-06-02 00:45:34 -0400

    Modificaciones en caché de Django -> dummy

--- second_project/settings.py
+++ second_project/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cambiando el caché a modo DESARROLLO con un archivo DUMMY
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
